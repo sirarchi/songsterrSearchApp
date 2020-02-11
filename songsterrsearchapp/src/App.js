01de09edc406b6caa1ff840c8465b63023a1c070
sirarchi <ddylak@intercars.eu>
2020-02-11 16:10:04 +0100

    Added title component.

--- songsterrsearchapp/src/App.js
+++ songsterrsearchapp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBox from './Components/SearchBox/SearchBox';
 import ScoreBoard from './Components/ScoreBoard/ScoreBoard';
+import Title from './Components/Title/Title';
 
 class App extends Component {
   state = {
@@ -17,6 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Title />
         <SearchBox insertTablatureToMainState={this.insertTablatureToMainState} />
         {this.state.tablatureData.length ? 
           (<ScoreBoard tablatureData={this.state.tablatureData} />) : 
---
+++ songsterrsearchapp/src/Components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div className="container-md text-center mt-3">
+            <h1 className="" >songsterrSearchApp</h1>
+            <p className="ml-5">powered by Songsterr</p>
+        </div>
+    )
+}
+
+export default Title
