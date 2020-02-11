186e05cce7117623f950a8075bc94f77b0154cf8
sirarchi <ddylak@intercars.eu>
2020-02-11 14:39:49 +0100

    Prepared filter function in ScoreBoard

--- songsterrsearchapp/src/Components/ScoreBoard/ScoreBoard.js
+++ songsterrsearchapp/src/Components/ScoreBoard/ScoreBoard.js
@@ -5,6 +5,7 @@ import Record from '../Record/Record'
 class ScoreBoard extends Component {
     state = {
         tablatureData: [],
+        tabTypes: 'PLAYER',
     }
 
     componentDidUpdate(previousProps, previousState) {
@@ -15,27 +16,53 @@ class ScoreBoard extends Component {
         }
     }
 
+    filterTabs = (e) => {
+        if(this.state.tabTypes !== e.target.value) {
+            this.setState({
+                tabTypes: e.target.value.toUpperCase()
+            })   
+        }
+  
+        console.log(this.filteredTabs);
+        
+    }
+
     render () {
+        let filteredTabs = this.state.tablatureData.filter((item) => {
+            return item.tabTypes.indexOf(this.state.tabTypes) !== -1
+        });
         return (
             <div className="container-md p-0">
                 <div className="row">
                     <table className="table table-striped">
                         <thead>
-                            <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Artist Name</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Type</th>
-                            <th scope="col">Tab type</th>
+                            <tr className="">
+                                <th className="align-middle" scope="col">ID</th>
+                                <th className="align-middle" scope="col">Artist Name</th>
+                                <th className="align-middle" scope="col">Title</th>
+                                <th className="align-middle" scope="col">Type</th>
+                                <th className="align-middle" scope="col">
+                                    <p className="text-center">TabType</p>
+                                    <div className="form-group mb-0">
+                                        <select onChange={this.filterTabs} className="form-control" id="exampleFormControlSelect1">
+                                            <option vale="player">player</option>
+                                            <option value="text_guitar_tab">guitar</option>
+                                            <option value="text_bass_tab">bass</option>
+                                            <option value="chords">chords</option>
+                                        </select>
+                                    </div>
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
                             { 
                                 this.props.tablatureData !== undefined ? 
-                                this.state.tablatureData.map(item => {
+                                filteredTabs.map(item => {
                                     return <Record key={item.id} tablatureData={item} />
                                 }) : 
-                                <p>Nothing to show!</p>
+                                <tr>
+                                    <th>Nothing to show!</th>
+                                </tr>
                             }
                         </tbody>
                     </table>
